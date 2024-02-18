ba9eeaa6a83aeb53de841d6485b4ea5f7a833031
violessi <108144748+violessi@users.noreply.github.com>
2024-02-19 03:01:15 +0800

    build: add linting rules
    Linting rules added for HTML, CSS, JS/TS, and Svelte.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { AppShell } from '@skeletonlabs/skeleton';
+    import '../app.postcss';
+    import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
 <!-- App Shell -->
 <AppShell>
-	<slot />
+    <slot />
 </AppShell>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,26 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import { CourseMinder } from './src/CourseMinder';
 import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import { join } from 'path';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { CourseMinder } from './src/CourseMinder'
+import typography from '@tailwindcss/typography';
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				custom: [
-					CourseMinder,
-				],
-			},
-		}),
-	],
+    darkMode: 'class',
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        forms,
+        typography,
+        skeleton({
+            themes: {
+                custom: [CourseMinder],
+            },
+        }),
+    ],
 } satisfies Config;
