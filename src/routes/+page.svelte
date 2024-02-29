c9608f5ff43725aaf740ae52cfc7c1ae1251baee
Jemimah Mae Isaig <jcisaig@up.edu.ph>
2024-02-29 18:03:12 +0800

    add Choice page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,21 @@
     // import icon from '$lib/assets/icon.png';
     
 </script>
+
+<body class="background">
+    <div class= "flex flex-col items-center justify-center h-screen">
+        <img src={icon2} alt="CourseMinder Logo" class="w-80 h-48 me-6" />
+        <h1 class="text-2xl font-michroma">CourseMinder</h1>
+        <h2 class="text-lg mb-6 font-noto">"Stay Ahead. &nbsp Stay Focused."</h2>
+        <div class="flex space-x-4">
+            <a href="/choice/login" class="text-white text-xl font-noto">Login</a>
+            <div class="border border-white ml-4 mt-3"></div>
+            <a href="/choice/signup" class="text-white text-xl font-noto">Sign Up</a>
+        </div>
+        <!-- <a href="/student/dashboard" class="btn text-black variant-filled-primary">Student Dashboard</a> -->
+    </div>  
+</body>
+
 <style>
     .font-michroma {
     font-family: "Michroma", sans-serif;
@@ -22,17 +37,3 @@
         background-attachment: fixed;
     }
 </style>
-
-<body class="background">
-    <div class= "flex flex-col items-center justify-center h-screen">
-        <img src={icon2} alt="CourseMinder Logo" class="w-80 h-48 me-6" />
-        <h1 class="text-2xl font-michroma">CourseMinder</h1>
-        <h2 class="text-lg mb-6 font-noto">"Stay Ahead. &nbsp Stay Focused."</h2>
-        <div class="flex space-x-4">
-            <a href="/choice" class="text-white text-xl font-noto">Login</a>
-            <div class="border border-white ml-4 mt-3"></div>
-            <a href="/choice" class="text-white text-xl font-noto">Sign Up</a>
-        </div>
-        <a href="/student/dashboard" class="btn text-black variant-filled-primary">Student Dashboard</a>
-    </div>  
-</body>
---
+++ src/routes/choice/login/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+        import icon2 from '$lib/assets/icon2.png';
+</script>
+
+<style>
+    .font-michroma {
+        font-family: "Michroma", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+    .font-moul {
+    font-family: "Moul", serif;
+    font-weight: 400;
+    font-style: normal;
+    }
+
+
+    .background{
+        background-image: url('./src/lib/assets/bg.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-attachment: fixed;
+    }
+
+    .black-film {
+    background: rgba(0, 0, 0, 0.5);
+    }
+</style>
+
+<body class="background">
+    <div class="black-film">
+        <p>&nbsp</p>
+        <img src={icon2} alt="Icon" class="w-20 h-11 ml-4 mb-0" />
+        <p class="font-michroma mt-0 text-[9px] ml-4">CourseMinder</p>
+        <div class= "flex flex-col items-center justify-center h-screen">
+            <p class="text-6xl font-moul">Are you a</p> 
+            <div class="grid grid-cols gap-4">
+                <a href="/student/login" class="btn variant-filled">Student</a>
+                <a href="/faculty/login" class="btn variant-filled">Faculty</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+
+
