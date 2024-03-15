574a7c12d32f29f045df0ccf67f78e617488ffb9
violessi <108144748+violessi@users.noreply.github.com>
2024-03-15 12:49:59 +0800

    chore

--- src/routes/(user)/student/curriculum/+page.svelte
+++ src/routes/(user)/student/curriculum/+page.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
-    import { initializeApp } from "firebase/app";
-    import { getDatabase, ref, push, get, child, set, query, equalTo, orderByValue, orderByKey } from "firebase/database"
+    import { initializeApp } from 'firebase/app';
+    import {
+        getDatabase,
+        ref,
+        push,
+        get,
+        child,
+        set,
+        query,
+        equalTo,
+        orderByValue,
+        orderByKey,
+    } from 'firebase/database';
     import { studentId, studentDegree } from '$lib/stores/CurriculumStores';
 
     const firebaseConfig = {
@@ -138,26 +149,74 @@
             'Soc Sci 2',
             'PI 100',
         ];
-    }
-    else if (degree === 'Civil Engineering'){
-        courses = ['Physics 71', 'Physics 71.1', 'Chem 16', 'Chem 16.1', 'Math 21', 'Eng 13', 'Speech 30',
-                    'Physics 72', 'Physics 72.1', 'ES 1', 'CE 29', 'Math 22', 'GE 10', 'CE 1', 'CE 41', 
-                    'CE 11', 'Math 23', 'CE 24', 'GE 12', 'ES 101', 'NSTP 1', 'CE 22', 'CE 31', 'CE 130', 
-                    'CE 17', 'CE 25', 'ES 102', 'NSTP 2', 'CE 123', 'CE 115', 'CE 141', 'CE 18', 'CE 151', 
-                    'CE 162', 'CE 124', 'CE 132', 'CE 116', 'CE 142', 'CE 152', 'CE 163', 'CE 190', 'CE 195',
-                    'CE 199', 'CE Elective 1', 'Kas 1', 'Fil 40', 'DRMAPS', 'GE Elective', 'CE 196',
-                    'CE Elective 2', 'Arts 1', 'Philo 1', 'Soc Sci 2', 'PI 100'];
+    } else if (degree === 'Civil Engineering') {
+        courses = [
+            'Physics 71',
+            'Physics 71.1',
+            'Chem 16',
+            'Chem 16.1',
+            'Math 21',
+            'Eng 13',
+            'Speech 30',
+            'Physics 72',
+            'Physics 72.1',
+            'ES 1',
+            'CE 29',
+            'Math 22',
+            'GE 10',
+            'CE 1',
+            'CE 41',
+            'CE 11',
+            'Math 23',
+            'CE 24',
+            'GE 12',
+            'ES 101',
+            'NSTP 1',
+            'CE 22',
+            'CE 31',
+            'CE 130',
+            'CE 17',
+            'CE 25',
+            'ES 102',
+            'NSTP 2',
+            'CE 123',
+            'CE 115',
+            'CE 141',
+            'CE 18',
+            'CE 151',
+            'CE 162',
+            'CE 124',
+            'CE 132',
+            'CE 116',
+            'CE 142',
+            'CE 152',
+            'CE 163',
+            'CE 190',
+            'CE 195',
+            'CE 199',
+            'CE Elective 1',
+            'Kas 1',
+            'Fil 40',
+            'DRMAPS',
+            'GE Elective',
+            'CE 196',
+            'CE Elective 2',
+            'Arts 1',
+            'Philo 1',
+            'Soc Sci 2',
+            'PI 100',
+        ];
     }
 
     let showPopup = false;
     let selectedCourse = '';
-    let courseTitle = ''
+    let courseTitle = '';
     let courseDescription = '';
     let numUnits = '';
     let prerequisites = '';
     let corequisites = '';
 
-    async function getCourseKey(course : string) {
+    async function getCourseKey(course: string) {
         console.log(course);
         const q = query(ref(db, 'courseMap'), orderByValue(), equalTo(course));
         const snapshot = await get(q);
@@ -174,10 +233,10 @@
         }
     }
 
-    async function getCourseData(courseKey : string | undefined) {
-        if (courseKey === null){
+    async function getCourseData(courseKey: string | undefined) {
+        if (courseKey === null) {
             console.log('courseKey is null');
-            return
+            return;
         }
         const courseRef = ref(db, 'courses/' + courseKey);
         const snapshot = await get(courseRef);
@@ -193,12 +252,11 @@
             console.log('No data available');
         }
     }
-    
 
-    async function showCoursePopup(course : string) {
+    async function showCoursePopup(course: string) {
         selectedCourse = course;
         showPopup = true;
-        let courseKey : string | undefined = await getCourseKey(course);
+        let courseKey: string | undefined = await getCourseKey(course);
         getCourseData(courseKey);
     }
 
@@ -218,40 +276,43 @@
                     desc: courseDescription,
                     numUnits: numUnits,
                     prereq: prerequisites,
-                    coreq: corequisites
+                    coreq: corequisites,
                 };
                 const newCourseRef = push(courseRef, courseData);
                 courseKey = newCourseRef.key;
                 const mapRef = ref(db, 'courseMap/' + courseKey);
                 set(mapRef, courses[i]);
-
             }
         }
     }
 
     // makeCourseDirectory();
 
-    async function deleteCollection(collectionPath : string) {
+    async function deleteCollection(collectionPath: string) {
         const courseref = ref(db, collectionPath);
         await set(courseref, null);
     }
 
     // deleteCollection('courses');
     // deleteCollection('courseMap')
-
-
 </script>
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 <div>
     <div class="flex-row grid grid-cols-4 p-2 grid-rows-5 gap-4 content-center">
         {#each courses as course (course)}
-            <button on:click={() => showCoursePopup(course)} class="bg-secondary-500 rounded-lg p-1.5 text-sm">{course}</button>
+            <button on:click={() => showCoursePopup(course)} class="bg-secondary-500 rounded-lg p-1.5 text-sm"
+                >{course}</button
+            >
         {/each}
     </div>
-    
+
     {#if showPopup}
         <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <div class="bg-white p-4 rounded left-7 w-1/2 items-center justify-center">
-                <h2 class="text-emerald-900 text-xl font-bold mb-2"> {selectedCourse}</h2>
+                <h2 class="text-emerald-900 text-xl font-bold mb-2">{selectedCourse}</h2>
                 <div class="flex-column">
                     <p class="text-emerald-900"><strong>Course Title:</strong> {courseTitle}</p>
                     <p class="text-emerald-900"><strong>Course Description:</strong> {courseDescription}</p>
