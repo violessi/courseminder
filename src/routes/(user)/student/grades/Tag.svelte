406b60f090719e6b31566ef5ccaac0385a6655f1
violessi <alessi.yukdawan@gmail.com>
2024-03-16 00:26:30 +0800

    feat: implement tag component

---
+++ src/routes/(user)/student/grades/Tag.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let label: string;
+    export let value: string | number;
+</script>
+
+<div class="flex items-center gap-4 font-bold">
+    <div class="text-2xl text-tertiary-700">{label}</div>
+    <div class="text-4xl text-tertiary-900">{value}</div>
+</div>
