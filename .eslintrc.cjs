c2b6d7c143da981833d9551c4601babc599c0a75
violessi <alessi.yukdawan@gmail.com>
2024-03-16 11:20:00 +0800

    refactor: prefer using modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -46,7 +46,6 @@ module.exports = {
         'dot-notation': 'error',
         eqeqeq: 'error',
         'func-style': ['error', 'declaration'],
-        'init-declarations': 'warn',
         'logical-assignment-operators': 'error',
         'new-cap': 'error',
         'no-alert': 'warn',
--- src/routes/(user)/student/curriculum/+page.svelte
+++ src/routes/(user)/student/curriculum/+page.svelte
@@ -1,117 +1,49 @@
 <script lang="ts">
-    import { equalTo, get, getDatabase, orderByValue, query, ref } from 'firebase/database';
     import { COURSES } from '$lib/data/courses';
-    import { initializeApp } from 'firebase/app';
+    import { Course } from '$lib/models/types';
     import { studentDegree } from '$lib/stores/CurriculumStores';
+    import { getCourseData, getCourseKey } from '$lib/firebase/database';
 
-    const firebaseConfig = {
-        apiKey: 'AIzaSyCmwpRzGyoeD-Xuh6Cuh1Agbsxw31Uekhk',
-        authDomain: 'courseminder-dev.firebaseapp.com',
-        databaseURL: 'https://courseminder-dev-default-rtdb.asia-southeast1.firebasedatabase.app',
-        projectId: 'courseminder-dev',
-        storageBucket: 'courseminder-dev.appspot.com',
-        messagingSenderId: '274860730108',
-        appId: '1:274860730108:web:b7f706a51ee7a79dbd1979',
-        measurementId: 'G-1T6H3BFHRR',
-    };
+    // FIXME: we need to initialize firebase at the root level
+    import { initFirebase } from '$lib/firebase/client';
+    initFirebase();
 
-    // Initialize Firebase and get database
-    const app = initializeApp(firebaseConfig);
-    const db = getDatabase(app);
-
-    // NOTE: currently unused
-    // let studentnumber;
-    // studentId.subscribe((value) => {
-    //     studentnumber = value;
-    // });
-
+    // fetch the courses for the student
     $: courses = COURSES[$studentDegree];
 
     let showPopup = false;
-    let selectedCourse = '';
-    let courseTitle = '';
-    let courseDescription = '';
-    let numUnits = '';
-    let prerequisites = '';
-    let corequisites = '';
-
-    async function getCourseKey(course: string) {
-        const q = query(ref(db, 'courseMap'), orderByValue(), equalTo(course));
-        const snapshot = await get(q);
-        if (snapshot.exists()) {
-            let courseKey;
-            snapshot.forEach((childSnapshot) => {
-                console.log('Child found');
-                courseKey = childSnapshot.key;
-            });
-            return courseKey;
-        }
-        console.log('No matching courses.');
-    }
-
-    async function getCourseData(courseKey: string | undefined) {
-        if (courseKey === null) {
-            console.log('courseKey is null');
-            return;
-        }
-        const courseRef = ref(db, `courses/${courseKey}`);
-        const snapshot = await get(courseRef);
-        if (snapshot.exists()) {
-            const courseData = snapshot.val();
-            ({ selectedCourse, courseTitle, courseDescription, numUnits, prerequisites, corequisites } = courseData);
-        } else {
-            console.log('No data available');
-        }
-    }
+    let selectedCourse: string;
+    let courseData: Course;
 
     async function showCoursePopup(course: string) {
         selectedCourse = course;
-        showPopup = true;
-        const courseKey: string | undefined = await getCourseKey(course);
-        getCourseData(courseKey);
+        try {
+            const courseKey = await getCourseKey(course);
+            courseData = await getCourseData(courseKey);
+            showPopup = true;
+        } catch (e) {
+            // TODO: handle errors here
+            console.error(e);
+            return;
+        }
     }
 
     function closePopup() {
         showPopup = false;
     }
 
-    // NOTE: currently unused
-    // async function makeCourseDirectory() {
-    //     const courseRef = ref(db, `courses`);
-    //     for (let i = 0; i < courses.length; i++) {
-    //         const snapshot = await get(child(courseRef, courses[i]));
-    //         let courseKey;
-    //         if (!snapshot.exists()) {
-    //             let courseData = {
-    //                 course: courses[i],
-    //                 title: courseTitle,
-    //                 desc: courseDescription,
-    //                 numUnits: numUnits,
-    //                 prereq: prerequisites,
-    //                 coreq: corequisites,
-    //             };
-    //             const newCourseRef = push(courseRef, courseData);
-    //             courseKey = newCourseRef.key;
-    //             const mapRef = ref(db, 'courseMap/' + courseKey);
-    //             set(mapRef, courses[i]);
-    //         }
-    //     }
-    // }
-
-    // NOTE: currently unused
-    // async function deleteCollection(collectionPath: string) {
-    //     const courseref = ref(db, collectionPath);
-    //     await set(courseref, null);
-    // }
+    // NOTE: Unused functions are found at the bottom of this file
 </script>
 
 <div>
     <div class="flex-row grid grid-cols-4 p-2 grid-rows-5 gap-4 content-center">
-        {#each courses as course (course)}
-            <button on:click={() => showCoursePopup(course)} class="bg-secondary-500 rounded-lg p-1.5 text-sm"
-                >{course}</button
-            >
-        {/each}
+        {#if courses}
+            {#each courses as course (course)}
+                <button on:click={() => showCoursePopup(course)} class="bg-secondary-500 rounded-lg p-1.5 text-sm"
+                    >{course}</button
+                >
+            {/each}
+        {/if}
     </div>
 
     {#if showPopup}
@@ -119,14 +51,52 @@
             <div class="bg-white p-4 rounded left-7 w-1/2 items-center justify-center">
                 <h2 class="text-emerald-900 text-xl font-bold mb-2">{selectedCourse}</h2>
                 <div class="flex-column">
-                    <p class="text-emerald-900"><strong>Course Title:</strong> {courseTitle}</p>
-                    <p class="text-emerald-900"><strong>Course Description:</strong> {courseDescription}</p>
-                    <p class="text-emerald-900"><strong>Number of Units:</strong> {numUnits}</p>
-                    <p class="text-emerald-900"><strong>Prerequisites:</strong> {prerequisites}</p>
-                    <p class="text-emerald-900"><strong>Corequisites:</strong> {corequisites}</p>
+                    <p class="text-emerald-900"><strong>Course Title:</strong> {courseData.title}</p>
+                    <p class="text-emerald-900">
+                        <strong>Course Description:</strong>
+                        {courseData.desc}
+                    </p>
+                    <p class="text-emerald-900"><strong>Number of Units:</strong> {courseData.numUnits}</p>
+                    <p class="text-emerald-900"><strong>Prerequisites:</strong> {courseData.prereq}</p>
+                    <p class="text-emerald-900"><strong>Corequisites:</strong> {courseData.coreq}</p>
                 </div>
                 <button on:click={closePopup} class="bg-red-500 text-white rounded px-2 py-1">Close</button>
             </div>
         </div>
     {/if}
 </div>
+
+<!-- // NOTE: currently unused
+let studentnumber;
+studentId.subscribe((value) => {
+    studentnumber = value;
+});
+
+// NOTE: currently unused
+async function makeCourseDirectory() {
+    const courseRef = ref(db, `courses`);
+    for (let i = 0; i < courses.length; i++) {
+        const snapshot = await get(child(courseRef, courses[i]));
+        let courseKey;
+        if (!snapshot.exists()) {
+            let courseData = {
+                course: courses[i],
+                title: courseTitle,
+                desc: courseDescription,
+                numUnits: numUnits,
+                prereq: prerequisites,
+                coreq: corequisites,
+            };
+            const newCourseRef = push(courseRef, courseData);
+            courseKey = newCourseRef.key;
+            const mapRef = ref(db, 'courseMap/' + courseKey);
+            set(mapRef, courses[i]);
+        }
+    }
+}
+
+// NOTE: currently unused
+async function deleteCollection(collectionPath: string) {
+    const courseref = ref(db, collectionPath);
+    await set(courseref, null);
+} -->
