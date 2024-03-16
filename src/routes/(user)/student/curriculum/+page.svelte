33d3271161311d2fe7792c0ad29137e6eaafa552
violessi <alessi.yukdawan@gmail.com>
2024-03-16 11:33:04 +0800

    refactor: prefer popup as component

--- src/routes/(user)/student/curriculum/+page.svelte
+++ src/routes/(user)/student/curriculum/+page.svelte
@@ -2,6 +2,7 @@
     import { getCourseData, getCourseKey } from '$lib/firebase/database';
     import { COURSES } from '$lib/data/courses';
     import { Course } from '$lib/models/types';
+    import Popup from './Popup.svelte';
     import { studentDegree } from '$lib/stores/CurriculumStores';
 
     // FIXME: we need to initialize firebase at the root level
@@ -28,41 +29,24 @@
         }
     }
 
-    function closePopup() {
-        showPopup = false;
-    }
-
     // NOTE: Unused functions are found at the bottom of this file
 </script>
 
 <div>
     <div class="flex-row grid grid-cols-4 p-2 grid-rows-5 gap-4 content-center">
         {#if courses}
-            {#each courses as course (course)}
+            {#each courses as course}
                 <button on:click={() => showCoursePopup(course)} class="bg-secondary-500 rounded-lg p-1.5 text-sm"
                     >{course}</button
                 >
             {/each}
+        {:else}
+            <p>No courses found</p>
         {/if}
     </div>
 
     {#if showPopup}
-        <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-            <div class="bg-white p-4 rounded left-7 w-1/2 items-center justify-center">
-                <h2 class="text-emerald-900 text-xl font-bold mb-2">{selectedCourse}</h2>
-                <div class="flex-column">
-                    <p class="text-emerald-900"><strong>Course Title:</strong> {courseData.title}</p>
-                    <p class="text-emerald-900">
-                        <strong>Course Description:</strong>
-                        {courseData.desc}
-                    </p>
-                    <p class="text-emerald-900"><strong>Number of Units:</strong> {courseData.numUnits}</p>
-                    <p class="text-emerald-900"><strong>Prerequisites:</strong> {courseData.prereq}</p>
-                    <p class="text-emerald-900"><strong>Corequisites:</strong> {courseData.coreq}</p>
-                </div>
-                <button on:click={closePopup} class="bg-red-500 text-white rounded px-2 py-1">Close</button>
-            </div>
-        </div>
+        <Popup {selectedCourse} {courseData} bind:showPopup />
     {/if}
 </div>
 
---
+++ src/routes/(user)/student/curriculum/Popup.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { Course } from '$lib/models/types';
+
+    export let selectedCourse: string;
+    export let courseData: Course;
+    export let showPopup: boolean;
+
+    $: ({ title, desc, numUnits, prereq, coreq } = courseData);
+</script>
+
+<div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div class="bg-white p-4 rounded left-7 w-1/2 items-center justify-center">
+        <h2 class="text-emerald-900 text-xl font-bold mb-2">{selectedCourse}</h2>
+        <div class="flex-column">
+            <p class="text-emerald-900"><strong>Course Title:</strong> {title}</p>
+            <p class="text-emerald-900">
+                <strong>Course Description:</strong>
+                {desc}
+            </p>
+            <p class="text-emerald-900"><strong>Number of Units:</strong> {numUnits}</p>
+            <p class="text-emerald-900"><strong>Prerequisites:</strong> {prereq}</p>
+            <p class="text-emerald-900"><strong>Corequisites:</strong> {coreq}</p>
+        </div>
+        <button on:click={() => (showPopup = false)} class="bg-red-500 text-white rounded px-2 py-1">Close</button>
+    </div>
+</div>
