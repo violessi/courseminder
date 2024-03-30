2d03406ee35108a9e8f4a7c3a339836edd628fd7
Masayuki Inukai <mzinukai@up.edu.ph>
2024-03-30 15:03:45 +0800

    feat: initialize store with data from database

--- src/lib/stores/SemesterStores.ts
+++ src/lib/stores/SemesterStores.ts
@@ -1,12 +1,58 @@
 import { AddSem, Semester, Subject } from '$lib/models/types';
 import { getContext, hasContext, setContext } from 'svelte';
 import { get as getStore, writable } from 'svelte/store';
+import { initializeApp } from 'firebase/app'
+import { getDatabase, ref, get as getData, set as setData } from 'firebase/database';
+import { studentId } from '$lib/stores/CurriculumStores';
 
 const SEMESTER = Symbol('semester');
+
+let studentnumber = '';
+studentId.subscribe((value) => {
+    studentnumber = value;
+});
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCmwpRzGyoeD-Xuh6Cuh1Agbsxw31Uekhk',
+    authDomain: 'courseminder-dev.firebaseapp.com',
+    databaseURL: 'https://courseminder-dev-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'courseminder-dev',
+    storageBucket: 'courseminder-dev.appspot.com',
+    messagingSenderId: '274860730108',
+    appId: '1:274860730108:web:b7f706a51ee7a79dbd1979',
+    measurementId: 'G-1T6H3BFHRR',
+};
+
+// Initialize Firebase and get database
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const reference = ref(db, `semesterData/${studentnumber}`);
+
+function parseSemester(sem : string, year : string){
+    const yearId = year.split('-');
+    const startYear = yearId[0].slice(2, 3);
+    const endYear = yearId[1].slice(2, 3);
+    let semId = startYear + endYear;
+    if (sem === '1st Semester') semId += 'A';
+    else if (sem === '2nd Semester') semId += 'B';
+    else semId += 'M';
+
+    return semId;
+}
 
 function initStore() {
     const store = writable<Semester[]>([]);
     const { subscribe, set, update } = store;
+
+    getData(reference).then(snapshot => {
+        const data : any[] = [];
+        snapshot.forEach(childSnapshot => {
+            const childData = childSnapshot.val();
+            data.push(childData);
+        });
+        console.log(data);
+        set(data);
+    });
 
     function addSem({ sem, year }: AddSem) {
         const obg = {
@@ -20,6 +66,18 @@ function initStore() {
             subjects: [],
         } satisfies Semester;
         update((store) => [...store, obg]);
+
+        // Update database
+        const semId = parseSemester(sem, year);
+        const reference = ref(db, `semesterData/${studentnumber}/${semId}`);
+        getData(reference).then(snapshot => {
+            if (snapshot.exists()){
+                console.log('Semester already exists');
+            }
+            else {
+                setData(reference, obg);
+            }
+        });
     }
 
     function getSem(id: string) {
