ae67382e0d9cfe453fec5c33224bc954d8fc4436
Jemimah Mae Isaig <jcisaig@up.edu.ph>
2024-05-17 03:02:39 +0800

    add: CE Course Graph

---
+++ src/lib/data/ceNodes.ts
@@ -0,0 +1,711 @@
+import { type Edge, type Node } from '@xyflow/svelte';
+
+export const ceInitialNodes: Node[]= [
+    {
+        id: 'Physics 71',
+        type: 'custom',
+        data: {name: 'Physics 71', height: 40, width: 115},
+        position: {x:258, y:133},
+        draggable: false,
+    },
+    {
+        id: 'Physics 72',
+        type: 'custom',
+        data: {name: 'Physics 72', height: 40, width: 115},
+        position: {x:258, y:212},
+        draggable: false,
+    },
+    {
+        id: 'Physics 711',
+        type: 'custom',
+        data: {name: 'Physics 711', height: 40, width: 115},
+        position: {x:385, y:133},
+        draggable: false,
+    },
+    {
+        id: 'Physics 721',
+        type: 'custom',
+        data: {name: 'Physics 721', height: 40, width: 115},
+        position: {x:385, y:212},
+        draggable: false,
+    },
+    {
+        id: 'CE 41',
+        type: 'custom',
+        data: {name: 'CE 41', height: 40, width: 115},
+        position: {x:343, y:321},
+        draggable: false,
+    },
+    {
+        id: 'CE 22',
+        type: 'custom',
+        data: {name: 'CE 22', height: 40, width: 115},
+        position: {x:243, y:403},
+        draggable: false,
+    },
+    {
+        id: 'CE 123',
+        type: 'custom',
+        data: {name: 'CE 123', height: 40, width: 115},
+        position: {x:390, y:476},
+        draggable: false,
+    },
+    {
+        id: 'CE 124',
+        type: 'custom',
+        data: {name: 'CE 124', height: 40, width: 115},
+        position: {x:333, y:587},
+        draggable: false,
+    },
+    {
+        id: 'Math 21',
+        type: 'custom',
+        data: {name: 'Math 21', height: 40, width: 115},
+        position: {x:771, y:133},
+        draggable: false,
+    },
+    {
+        id: 'ES 1',
+        type: 'custom',
+        data: {name: 'ES 21', height: 40, width: 115},
+        position: {x:537, y:212},
+        draggable: false,
+    },
+    {
+        id: 'CE 29',
+        type: 'custom',
+        data: {name: 'CE 29', height: 40, width: 115},
+        position: {x:568, y:283},
+        draggable: false,
+    },
+    {
+        id: 'CE 31',
+        type: 'custom',
+        data: {name: 'CE 31', height: 40, width: 115},
+        position: {x:478, y:383},
+        draggable: false,
+    },
+    {
+        id: 'Math 22',
+        type: 'custom',
+        data: {name: 'Math 22', height: 40, width: 115},
+        position: {x:771, y:219},
+        draggable: false,
+    },
+    {
+        id: 'CE 24',
+        type: 'custom',
+        data: {name: 'CE 24', height: 40, width: 115},
+        position: {x:771, y:303},
+        draggable: false,
+    },
+    {
+        id: 'CE 25',
+        type: 'custom',
+        data: {name: 'CE 25', height: 40, width: 115},
+        position: {x:771, y:377},
+        draggable: false,
+    },
+    {
+        id: 'CE 141',
+        type: 'custom',
+        data: {name: 'CE 141', height: 40, width: 115},
+        position: {x:771, y:459},
+        draggable: false,
+    },
+    {
+        id: 'CE 142',
+        type: 'custom',
+        data: {name: 'CE 142', height: 40, width: 115},
+        position: {x:771, y:530},
+        draggable: false,
+    },
+    {
+        id: 'ES 101',
+        type: 'custom',
+        data: {name: 'ES 101', height: 40, width: 115},
+        position: {x:619, y:376},
+        draggable: false,
+    },
+    {
+        id: 'ES 102',
+        type: 'custom',
+        data: {name: 'ES 102', height: 40, width: 115},
+        position: {x:583, y:471},
+        draggable: false,
+    },
+    {
+        id: 'CE 18',
+        type: 'custom',
+        data: {name: 'CE 18', height: 40, width: 115},
+        position: {x:505, y:573},
+        draggable: false,
+    },
+    {
+        id: 'CE 151',
+        type: 'custom',
+        data: {name: 'CE 151', height: 40, width: 115},
+        position: {x:647, y:575},
+        draggable: false,
+    },
+    {
+        id: 'CE 152',
+        type: 'custom',
+        data: {name: 'CE 152', height: 40, width: 115},
+        position: {x:582, y:660},
+        draggable: false,
+    },
+    {
+        id: 'GE 10',
+        type: 'custom',
+        data: {name: 'GE 10', height: 40, width: 115},
+        position: {x:970, y:212},
+        draggable: false,
+    },
+    {
+        id: 'GE 12',
+        type: 'custom',
+        data: {name: 'GE 12', height: 40, width: 115},
+        position: {x:1040, y:288},
+        draggable: false,
+    },
+    {
+        id: 'CE 17',
+        type: 'custom',
+        data: {name: 'CE 17', height: 40, width: 115},
+        position: {x:1040, y:395},
+        draggable: false,
+    },
+    {
+        id: 'Math 23',
+        type: 'custom',
+        data: {name: 'Math 23', height: 40, width: 115},
+        position: {x:896, y:288},
+        draggable: false,
+    },
+    {
+        id: 'Chem 16',
+        type: 'custom',
+        data: {name: 'Chem 16', height: 40, width: 115},
+        position: {x:1052, y:133},
+        draggable: false,
+    },
+    {
+        id: 'Chem 161',
+        type: 'custom',
+        data: {name: 'Chem 161', height: 40, width: 115},
+        position: {x:1176, y:133},
+        draggable: false,
+    },
+    {
+        id: 'CE 11',
+        type: 'custom',
+        data: {name: 'CE 11', height: 40, width: 115},
+        position: {x:1300, y:133},
+        draggable: false,
+    },
+    {
+        id: 'CE 130',
+        type: 'custom',
+        data: {name: 'CE 130', height: 40, width: 115},
+        position: {x:1187, y:361},
+        draggable: false,
+    },
+    {
+        id: 'CE 190',
+        type: 'custom',
+        data: {name: 'CE 190', height: 40, width: 115},
+        position: {x:980, y:510},
+        draggable: false,
+    },
+    {
+        id: 'CE 199',
+        type: 'custom',
+        data: {name: 'CE 199', height: 40, width: 115},
+        position: {x:980, y:574},
+        draggable: false,
+    },
+    {
+        id: 'CE 162',
+        type: 'custom',
+        data: {name: 'CE 162', height: 40, width: 115},
+        position: {x:868, y:635},
+        draggable: false,
+    },
+    {
+        id: 'CE 163',
+        type: 'custom',
+        data: {name: 'CE 163', height: 40, width: 115},
+        position: {x:858, y:741},
+        draggable: false,
+    },
+    {
+        id: 'CE 132',
+        type: 'custom',
+        data: {name: 'CE 132', height: 40, width: 115},
+        position: {x:1132, y:505},
+        draggable: false,
+    },
+    {
+        id: 'CE 115',
+        type: 'custom',
+        data: {name: 'CE 115', height: 40, width: 115},
+        position: {x:1300, y:512},
+        draggable: false,
+    },
+    {
+        id: 'CE 116',
+        type: 'custom',
+        data: {name: 'CE 116', height: 40, width: 115},
+        position: {x:1300, y:660},
+        draggable: false,
+    },
+    {
+        id: 'CE 1',
+        type: 'custom',
+        data: {name: 'CE 1', height: 40, width: 115},
+        position: {x:1099, y:660},
+        draggable: false,
+    },
+    {
+        id: 'CE 195',
+        type: 'custom',
+        data: {name: 'CE 195', height: 40, width: 115},
+        position: {x:276, y:676},
+        draggable: false,
+    },
+    {
+        id: 'CE 196',
+        type: 'custom',
+        data: {name: 'CE 196', height: 40, width: 115},
+        position: {x:399, y:676},
+        draggable: false,
+    },
+    {
+        id: 'CE Elective 1',
+        type: 'custom',
+        data: {name: 'CE Elective 1', height: 40, width: 130},
+        position: {x:315, y:740},
+        draggable: false,
+    },
+    {
+        id: 'CE Elective 2',
+        type: 'custom',
+        data: {name: 'CE Elective 2', height: 40, width: 130},
+        position: {x:474, y:740},
+        draggable: false,
+    },
+    {
+        id: 'Kas 1',
+        type: 'custom',
+        data: {name: 'Kas 1', height: 40, width: 100},
+        position: {x:530, y:829},
+        draggable: false,        
+    },
+    {
+        id: 'Philo 1',
+        type: 'custom',
+        data: {name: 'Philo 1', height: 40, width: 100},
+        position: {x:650, y:829},
+        draggable: false,        
+    },
+    {
+        id: 'Soc Sci 1 or 2',
+        type: 'custom',
+        data: {name: 'Soc Sci 1 or 2', height: 40, width: 135},
+        position: {x:780, y:829},
+        draggable: false,        
+    },
+    {
+        id: 'Eng 13',
+        type: 'custom',
+        data: {name: 'Eng 13', height: 40, width: 100},
+        position: {x:940, y:829},
+        draggable: false,        
+    },
+    {
+        id: 'Fil 40',
+        type: 'custom',
+        data: {name: 'Fil 40', height: 40, width: 100},
+        position: {x:1060, y:829},
+        draggable: false,        
+    },
+    {
+        id: 'DRMAPS',
+        type: 'custom',
+        data: {name: 'DRMAPS', height: 40, width: 115},
+        position: {x:500, y:884},
+        draggable: false,        
+    },
+    {
+        id: 'GE Elective',
+        type: 'custom',
+        data: {name: 'GE Elective', height: 40, width: 120},
+        position: {x:645, y:884},
+        draggable: false,        
+    },
+    {
+        id: 'Arts 1',
+        type: 'custom',
+        data: {name: 'Arts 1', height: 40, width: 100},
+        position: {x:790, y:884},
+        draggable: false,        
+    },
+    {
+        id: 'PI 100',
+        type: 'custom',
+        data: {name: 'PI 100', height: 40, width: 100},
+        position: {x:920, y:884},
+        draggable: false,        
+    },
+    {
+        id: 'Free Elective',
+        type: 'custom',
+        data: {name: 'Free Elective', height: 40, width: 130},
+        position: {x:1050, y:884},
+        draggable: false,        
+    },
+    {
+        id: 'NSTP 1',
+        type: 'custom',
+        data: {name: 'NSTP 1', height: 40, width: 100},
+        position: {x: 700, y:939},
+        draggable: false,        
+    },
+    {
+        id: 'NSTP 2',
+        type: 'custom',
+        data: {name: 'NSTP 2', height: 40, width: 100},
+        position: {x:830, y:939},
+        draggable: false,        
+    },
+]
+
+export const ceInitialEdges: Edge[]= [
+    {
+        id: 'eP71-P72',
+        type: 'default',
+        source:'Physics 71',
+        target: 'Physics 72',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eP71.1-P72.2',
+        type: 'default',
+        source:'Physics 711',
+        target: 'Physics 721',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eP72-CE41',
+        type: 'default',
+        source:'Physics 72',
+        target: 'CE 41',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE41-CE123',
+        type: 'default',
+        source:'CE 41',
+        target: 'CE 123',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE123-CE124',
+        type: 'default',
+        source:'CE 123',
+        target: 'CE 124',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE22-CE124',
+        type: 'default',
+        source:'CE 22',
+        target: 'CE 124',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM21-ES1',
+        type: 'default',
+        source:'Math 21',
+        target: 'ES 1',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eES1-CE41',
+        type: 'default',
+        source:'ES 1',
+        target: 'CE 41',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM21-CE29',
+        type: 'default',
+        source:'Math 21',
+        target: 'CE 29',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE29-CE31',
+        type: 'default',
+        source:'CE 29',
+        target: 'CE 31',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE31-CE123',
+        type: 'default',
+        source:'CE 31',
+        target: 'CE 123',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM21-M22',
+        type: 'default',
+        source:'Math 21',
+        target: 'Math 22',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM22-CE24',
+        type: 'default',
+        source:'Math 22',
+        target: 'CE 24',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE24-CE25',
+        type: 'default',
+        source:'CE 24',
+        target: 'CE 25',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE25-CE141',
+        type: 'default',
+        source:'CE 25',
+        target: 'CE 141',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE141-CE142',
+        type: 'default',
+        source:'CE 141',
+        target: 'CE 142',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE29-CE141',
+        type: 'default',
+        source:'CE 29',
+        target: 'CE 141',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM22-ES101',
+        type: 'default',
+        source:'Math 22',
+        target: 'ES 101',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eES101-ES102',
+        type: 'default',
+        source:'ES 101',
+        target: 'ES 102',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eES102-CE18',
+        type: 'default',
+        source:'ES 102',
+        target: 'CE 18',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eES102-CE151',
+        type: 'default',
+        source:'ES 102',
+        target: 'CE 151',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE151-CE152',
+        type: 'default',
+        source:'CE 151',
+        target: 'CE 152',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM21-GE10',
+        type: 'default',
+        source:'Math 21',
+        target: 'GE 10',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eGE10-GE12',
+        type: 'default',
+        source:'GE 10',
+        target: 'GE 12',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eGE12-CE142',
+        type: 'default',
+        source:'GE 12',
+        target: 'CE 142',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM21-M23',
+        type: 'default',
+        source:'Math 21',
+        target: 'Math 23',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM23-CE25',
+        type: 'default',
+        source:'Math 23',
+        target: 'CE 25',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eM23-CE17',
+        type: 'default',
+        source:'Math 23',
+        target: 'CE 17',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eChem16-CE130',
+        type: 'default',
+        source:'Chem 16',
+        target: 'CE 130',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eChem161-CE130',
+        type: 'default',
+        source:'Chem 161',
+        target: 'CE 130',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE11-CE130',
+        type: 'default',
+        source:'CE 11',
+        target: 'CE 130',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE190-CE199',
+        type: 'default',
+        source:'CE 190',
+        target: 'CE 199',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE11-CE162',
+        type: 'default',
+        source:'CE 11',
+        target: 'CE 162',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eES102-CE162',
+        type: 'default',
+        source:'ES 102',
+        target: 'CE 162',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE162-CE163',
+        type: 'default',
+        source:'CE 162',
+        target: 'CE 163',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE11-CE115',
+        type: 'default',
+        source:'CE 11',
+        target: 'CE 115',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE115-CE116',
+        type: 'default',
+        source:'CE 115',
+        target: 'CE 116',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE17-CE115',
+        type: 'default',
+        source:'CE 17',
+        target: 'CE 115',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE17-CE132',
+        type: 'default',
+        source:'CE 17',
+        target: 'CE 132',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    {
+        id: 'eCE130-CE132',
+        type: 'default',
+        source:'CE 130',
+        target: 'CE 132',
+        animated: true,
+        style: 'stroke: #000000;'
+    },
+    
+]
+
+for (const node of ceInitialNodes) {
+    node.data.isSource = ceInitialEdges.some(edge => edge.source === node.id);
+    node.data.isTarget = ceInitialEdges.some(edge => edge.target === node.id);
+  }
